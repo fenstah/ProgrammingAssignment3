b6a5e88e81c8d226d38c04a150deba7fd457cfb9
lfenster <lfenster@microsoft.com>
2014-07-16 10:39:58 -0400

    object for holding and caching outcome data with helper functions
    reduces need to continuously read file with helper functions to sort data depending on requested outcome

---
+++ cleanedData.R
@@ -0,0 +1,31 @@
+## The cleanAndCacheOutcomeData function creates an object that can cache the outcome data as well as clean it for 
+## only the columns we care about and quickly return an ordered version of the data given the outcome variable desired
+cleanAndCacheOutcomeData <- function(x = data.frame()) {
+        cleanedData <- ifelse(ncol(x)>=23, x[,c(2,7,11,17,23)], NULL)     #initialize cleanedData variable to NULL
+        set <- function(y) {                                              #public set method for initializing variable
+            x <<- y
+            cleanedData <- x[,c(2,7,11,17,23)]
+        }
+        get <- function() x                         #public get that returns the original outcomedata
+        getCleanedData <- function() {              #public getCleanedData that returns the cleanedData
+            if (!is.null(x)) x[,c(2,7,11,17,23)]
+        }
+        orderbyOutcome <- function(outcomeNum) {
+            data <- getCleanedData()
+            data<-data[order(data$State, data[,outcomeNum], data$Hospital.Name, decreasing=F),]    #get the ordered matrix by state, outcome and name 
+            data[,c(1:2,outcomeNum)]            
+        }
+        orderedByHeartAttack <- function() {
+            orderbyOutcome(3)
+        }
+        orderedByHeartFailure <- function() {
+            orderbyOutcome(4)
+        }
+        orderedByPneumonia <- function() {
+            orderbyOutcome(5)
+        }
+        list(set=set,get=get, getCleanedData=getCleanedData,
+             orderedByHeartAttack=orderedByHeartAttack,
+             orderedByHeartFailure=orderedByHeartFailure,
+             orderedByPneumonia=orderedByPneumonia) 
+}
